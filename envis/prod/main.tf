24ecc31845a40c7d2d6ec61e0014b96bbfed2e53
gordon_west@CLOUD-DevOps-ENGINEER <gordonwest08@gmail.com>
2026-02-13 23:21:29 +0300

    edited bucket name

--- envis/prod/main.tf
+++ envis/prod/main.tf
@@ -22,7 +22,7 @@ provider "aws" {
 module "s3_bucket" {
   source = "../../modules/s3-bucket"
 
-  bucket_name         = "my-company-prod-assets36880850"
+  bucket_name         = "my-company-prod-assets3-910541869155"
   enable_versioning   = true
   enable_encription   = true
   allow_public_access = false
